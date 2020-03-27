32591ca7b1e3ed4c2c081a01d448e97454e8266d
AUTOBVT-FTKGAVP\Administrator <1712073952@qq.com>
2020-03-27 18:00:27 +0800

    订单代码完成1.0

--- pay-com/src/main/java/commitPay.java
+++ pay-com/src/main/java/commitPay.java
@@ -9,5 +9,6 @@ public class commitPay {
     public static void main(String[] args) {
         System.out.println("支付模块");
         System.out.println("支付模块代码已经写完了");
+        System.out.println("支付代码进行了更新");
     }
 }
