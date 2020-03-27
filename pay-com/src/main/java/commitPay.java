51fdf335ad8f5ddffe674584ceb48ebddcba5e6e
AUTOBVT-FTKGAVP\Administrator <1712073952@qq.com>
2020-03-27 17:07:17 +0800

    测试代码

---
+++ pay-com/src/main/java/commitPay.java
@@ -0,0 +1,12 @@
+/**
+ * commitPay
+ *
+ * @Author: 雷孝权
+ * @CreateTime: 2020-03-27
+ * @Description:
+ */
+public class commitPay {
+    public static void main(String[] args) {
+        System.out.println("支付模块");
+    }
+}
